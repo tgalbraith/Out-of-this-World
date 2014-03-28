7338a2ecb76f6673aa62373a00f1efcb0d2dba32
tgalbraith <tg@Stannas-MacBook-Air.local>
2014-03-28 16:03:21 +0700

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this World
+//
+//  Created by tg on 3/28/14.
+//  Copyright (c) 2014 tgalbraith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
